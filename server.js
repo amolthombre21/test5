60bf92c1027960c0916e906a2094b35963c6f085
amolthombre21 <amolthombre21@gmail.com>
2014-12-12 21:40:40 -0800

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var app = express();
 var server = http.createServer(app);
 // all environments
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
-var server_ip_address = process.env.OPENSHIFT_NODEJS_PORT || '127.0.0.1'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 app.set('port', server_port);
 //app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 //app.set('port', process.env.PORT || 8080);
